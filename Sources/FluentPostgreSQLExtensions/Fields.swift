81dd38f5955ba38e5457485eaf8c6a8bb28af2e7
David Monagle <david@monagle.com.au>
2017-11-16 16:20:18 +1100

    Removed throws from createAutoTimestamps

--- Sources/FluentPostgreSQLExtensions/Fields.swift
+++ Sources/FluentPostgreSQLExtensions/Fields.swift
@@ -4,7 +4,7 @@ extension Builder {
     /// Convenience to create timestamp fields with triggers (PostgreSQL Only?)
     /// This has the minor side effect that every time this is called it updates the function that sets the timestamp
     /// It shouldn't do any damage, it's just slightly suboptimal when running preparations.
-    public func createAutoTimestamps() throws {
+    public func createAutoTimestamps() {
         custom("created_at", type: "TIMESTAMP WITH TIME ZONE", optional: false, unique: false, default: "now()")
         custom("updated_at", type: "TIMESTAMP WITH TIME ZONE", optional: true, unique: false)
         self.raw("CREATE TRIGGER \(self.entity.entity)_updated_at_trigger BEFORE UPDATE ON \(self.entity.entity) FOR EACH ROW EXECUTE PROCEDURE set_updated_at();")
